061ab0852ea137de9b0c34d3a9affec94c0b5d31
24apurv <24apurv.deshpande@gmail.com>
2019-10-18 23:51:24 +0530

    181019

--- src/gui/splashScreen/TitanX.java
+++ src/gui/splashScreen/TitanX.java
@@ -1,20 +1,29 @@
 
 package gui.splashScreen;
 
+import com.db.DatabaseConnection;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class TitanX extends Application {
     
     public static Stage stage = null;
+    private boolean flag;
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
+        AnchorPane root = FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
         
         Scene scene = new Scene(root);
         
@@ -24,7 +33,44 @@ public class TitanX extends Application {
         stage.setScene(scene);
         stage.show();
         
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(5), root);
+        fadeIn.setFromValue(0.5);
+        fadeIn.setToValue(1);
+        fadeIn.setCycleCount(1);
+        
+        fadeIn.play();
+        
+        fadeIn.setOnFinished((e) -> {
+               try {
+            DatabaseConnection.makeConnection();
+            flag=true;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SplashScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Parent fxml;
+            try {
+                fxml = FXMLLoader.load(getClass().getResource("/gui/error/InternetError.fxml"));
+            
+            root.getChildren().removeAll();
+            root.getChildren().setAll(fxml);
+            flag=false;
+            } catch (IOException ex1) {
+                Logger.getLogger(SplashScreenController.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
+               if(flag){
+            try {
+            Parent fxml = FXMLLoader.load(getClass().getResource("/gui/login/Login.fxml"));
+            root.getChildren().removeAll();
+            root.getChildren().setAll(fxml);
+        } catch (IOException ex) {
+            Logger.getLogger(SplashScreenController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         this.stage=stage;
+               }
+        });
+
+     
     }
 
 
